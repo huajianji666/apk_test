7a9569d1b4ecbf03d21114f8534f566c2db7d9f4
huajianji666 <159273944+huajianji666@users.noreply.github.com>
2026-02-06 14:51:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 
-class HelloApp(App):
+class TestApp(App):
     def build(self):
-        box = BoxLayout(orientation='vertical')
-        btn = Button(text='Hello from Kivy → APK',
-                     font_size='24sp',
-                     size_hint=(1, .3))
-        btn.bind(on_release=lambda _: setattr(btn, 'text', 'Clicked!'))
-        box.add_widget(btn)
-        return box
+        return Label(text="Hello Kivy! 打包成功 🎉", font_size=40)
 
-if __name__ == '__main__':
-    HelloApp().run()
+if __name__ == "__main__":
+    TestApp().run()
